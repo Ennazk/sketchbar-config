2400c9e2baeb74368f87daac477604979bb2f19d
Yassir Ennazk <yassir@ennazk.com>
2025-07-19 21:31:39 +0200

    Refactor volume scripts for improved widget handling and error detection

--- items/volume.sh
+++ items/volume.sh
@@ -2,4 +2,5 @@
 
 sketchybar --add item volume right \
            --set volume script="$PLUGIN_DIR/volume.sh" \
+                        update_freq=2 \
            --subscribe volume volume_change
--- plugins/volume.sh
+++ plugins/volume.sh
@@ -3,12 +3,20 @@
 # The volume_change event supplies a $INFO variable in which the current volume
 # percentage is passed to the script.
 
-# Get current volume if no event is triggered
 if [ "$SENDER" = "volume_change" ]; then
   VOLUME=$INFO
 else
-  VOLUME=$(osascript -e "output volume of (get volume settings)")
+  widget
+  VOLUME=$(osascript -e "output volume of (get volume settings)" 2>/dev/null)
+  if [ "$VOLUME" = "missing value" ] || [ -z "$VOLUME" ]; then
+    # Can't detect volume - hide the widget
+    sketchybar --set $NAME drawing=off
+    exit 0
+  fi
 fi
+
+# Show the widget and update it
+sketchybar --set $NAME drawing=on
 
 case $VOLUME in
   [6-9][0-9]|100) ICON="􀊩"
@@ -21,21 +29,3 @@ case $VOLUME in
 esac
 
 sketchybar --set $NAME icon="$ICON" label="$VOLUME%"
-fiume_change event supplies a $INFO variable in which the current volume
-# percentage is passed to the script.
-
-if [ "$SENDER" = "volume_change" ]; then
-  VOLUME="$INFO"
-
-  case "$VOLUME" in
-    [6-9][0-9]|100) ICON="󰕾"
-    ;;
-    [3-5][0-9]) ICON="󰖀"
-    ;;
-    [1-9]|[1-2][0-9]) ICON="󰕿"
-    ;;
-    *) ICON="󰖁"
-  esac
-
-  sketchybar --set "$NAME" icon="$ICON" label="$VOLUME%"
-fi
